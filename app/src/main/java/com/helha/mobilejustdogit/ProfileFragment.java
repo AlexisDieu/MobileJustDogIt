9e89651491faa569a2f211a9fe452f6930a60824
Nathan <73165547+Nathan@users.noreply.github.com>
2020-12-17 17:36:08 +0100

    Profile

--- app/src/main/java/com/helha/mobilejustdogit/ProfileFragment.java
+++ app/src/main/java/com/helha/mobilejustdogit/ProfileFragment.java
@@ -1,21 +1,30 @@
 package com.helha.mobilejustdogit;
 
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class ProfileFragment extends Fragment {
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    private TextInputEditText textOldPassword, textNewPassword, textConfirmPassword, textFirstname, textLastname, textVille, textCp, textNumero, textRue;
+    private String oldPassword, newPassword, confirmPassword, firstname, lastname;
 
-
-    }
+    private ProgressDialog loading;
+    private AlertDialog dialog;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -23,6 +32,30 @@ public class ProfileFragment extends Fragment {
         // Inflate the layout for this fragment
         View root = inflater.inflate(R.layout.profile_fragment, container, false);
 
-        return  root;
+
+        textOldPassword = (TextInputEditText) root.findViewById(R.id.AncienMotDePasse);
+        textVille = (TextInputEditText) root.findViewById(R.id.Ville);
+        textCp = (TextInputEditText) root.findViewById(R.id.cp);
+        textNumero = (TextInputEditText) root.findViewById(R.id.Numero);
+        textRue = (TextInputEditText) root.findViewById(R.id.rue);
+        textNewPassword = (TextInputEditText) root.findViewById(R.id.Nouveau_MotDePasse);
+        textConfirmPassword = (TextInputEditText) root.findViewById(R.id.ConfirmerMotDePasse);
+
+        textFirstname = (TextInputEditText) root.findViewById(R.id.text_firstname);
+        textLastname = (TextInputEditText) root.findViewById(R.id.text_lastname);
+
+
+        // fetch user and show profile data
+        return root;
     }
+
+    public void ChangerMotDePasse() {
+
+    }
+
+    public void MettreAJourProfile() {
+
+    }
+
+
 }
